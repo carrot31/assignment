b1780235dff0c8074b541130eb6386bac084a8ed
carrot31 <jungwon0508@naver.com>
2022-06-29 23:55:34 +0900

    first setting

---
+++ src/redux/configStore.js
@@ -0,0 +1,37 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+import main from "./modules/main";
+
+export const history = createBrowserHistory();
+const rootReducer = combineReducers({
+  main: main,
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history: history })];
+
+// 개발환경 선언
+const env = process.env.NODE_ENV;
+
+// 개발환경에서 로거 사용
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+//require 개발환경에서만 이용 import랑 비슷한 역할
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+let store = (initialStore) => createStore(rootReducer, enhancer);
+
+export default store();
---
+++ src/shared/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configStore";
+import Main from "../pages/Main";
+import NotFound from "../pages/NotFound";
+
+function App() {
+  return (
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </ConnectedRouter>
+  );
+}
+export default App;
