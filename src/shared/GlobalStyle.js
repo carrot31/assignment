e5c7cc3e4664427cfeda79f584e94c27b53bef59
carrot31 <100031041+carrot31@users.noreply.github.com>
2022-06-30 14:37:58 +0900

    Update GlobalStyle.js

--- src/shared/GlobalStyle.js
+++ src/shared/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 * {
-  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
   box-sizing: border-box;
   margin: 0;
   padding: 0;
